2cec132dc450b75368d83046343e6304086d1728
vinoddsce <vinod.dsce@gmail.com>
2019-09-15 18:16:09 +0530

    String Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-app-nisha';
+  title: string = 'This Is My First Angular App !!!!!';
 }
